29ffa6f4fffa90216fa9a291ea2e02df049b19a8
listanegra <vitormac3@hotmail.com>
2021-02-02 12:11:26 -0300

    Adicionado implementação Socket.IO

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
-import express from 'express';
-import routes from './routes';
+import http from 'http'
+import { Server, Socket } from 'socket.io'
+
+import express from 'express'
+import routes from './routes'
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    transports: ['websocket']
+});
+
+io.on('connection', (socket: Socket) => {
+    socket.on('message', (message: string) => {
+        console.log(message);
+    });
+
+    socket.on('join', () => { });
+});
+
 app.use(express.json());
 app.use(routes);
-app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333');
+
+server.listen(3333, () => {
+    console.log('🚀 Server started on port 3333');
 });
